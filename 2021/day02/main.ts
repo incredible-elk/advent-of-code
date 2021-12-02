512eef948cc87109889497b72a0d6acf3c612c2e
Stephanie Geuder <37314329+incredible-elk@users.noreply.github.com>
2021-12-02 21:57:41 +0100

    refactor: clean up code and improve naming

--- 2021/day02/main.ts
+++ 2021/day02/main.ts
@@ -9,66 +9,66 @@ const sumArray = (array: Array<number>) => {
   return distanceSum;
 };
 
-const extractNumberFromCourseValue = (courseValue: string) =>
-  parseInt(courseValue.slice(courseValue.indexOf(" ") + 1), 10);
+const extractNumberFromPathSegment = (pathSegment: string) =>
+  parseInt(pathSegment.slice(pathSegment.indexOf(" ") + 1), 10);
 
 // --- EXAMPLE PART 1 --- //
 
-const exampleCourse = "forward 5, down 5, forward 8, up 3, down 8, forward 2";
-const exampleCourseArray = exampleCourse.split(", ");
+const examplePath = "forward 5, down 5, forward 8, up 3, down 8, forward 2";
+const examplePathArray = examplePath.split(", ");
 
-const sumUpCourseValues = (
-  courseArray: Array<string>,
-  courseValueStartsWith: string
+const sumUpPathSegments = (
+  pathArray: Array<string>,
+  pathSegmentStartsWith: string
 ) => {
-  const filteredCourseValues = courseArray.filter((courseValue) =>
-    courseValue.startsWith(courseValueStartsWith)
+  const filteredPathSegments = pathArray.filter((pathSegment) =>
+    pathSegment.startsWith(pathSegmentStartsWith)
   );
-  const numbers = filteredCourseValues.map(extractNumberFromCourseValue);
+  const numbers = filteredPathSegments.map(extractNumberFromPathSegment);
   return sumArray(numbers);
 };
 
-const newSubmarinePosition = (courseArray: Array<string>) => {
-  const sumDistance = sumUpCourseValues(courseArray, "forward ");
-  const sumUpDepth = sumUpCourseValues(courseArray, "up ");
-  const sumDownDepth = sumUpCourseValues(courseArray, "down ");
+const newSubmarinePosition = (pathArray: Array<string>) => {
+  const sumDistance = sumUpPathSegments(pathArray, "forward ");
+  const sumUpDepth = sumUpPathSegments(pathArray, "up ");
+  const sumDownDepth = sumUpPathSegments(pathArray, "down ");
   const sumDepth = sumDownDepth - sumUpDepth;
   return sumDistance * sumDepth;
 };
-console.log(newSubmarinePosition(exampleCourseArray));
+console.log(newSubmarinePosition(examplePathArray));
 
 // --- PART 1 --- //
 
-const course = await Deno.readTextFile("./input.txt");
-const inputCourseArray = course.split("\n");
+const path = await Deno.readTextFile("./input.txt");
+const inputPathArray = path.split("\n");
 
-console.log(newSubmarinePosition(inputCourseArray));
+console.log(newSubmarinePosition(inputPathArray));
 
 // --- EXAMPLE PART 2 --- //
 
-const finalSubmarinePosition = (array: Array<string>) => {
+const finalSubmarinePosition = (pathArray: Array<string>) => {
   let horizontalDistance = 0;
   let aim = 0;
   let depth = 0;
 
-  for (let i = 0; i < array.length; i++) {
-    const courseValue = array[i];
-    const courseValueNumber = extractNumberFromCourseValue(courseValue);
+  for (let i = 0; i < pathArray.length; i++) {
+    const pathSegment = pathArray[i];
+    const pathSegmentValue = extractNumberFromPathSegment(pathSegment);
 
-    if (courseValue.startsWith("forward ")) {
-      horizontalDistance += courseValueNumber;
-      depth += courseValueNumber * aim;
-    } else if (courseValue.startsWith("up ")) {
-      aim -= courseValueNumber;
-    } else if (courseValue.startsWith("down ")) {
-      aim += courseValueNumber;
+    if (pathSegment.startsWith("forward ")) {
+      horizontalDistance += pathSegmentValue;
+      depth += pathSegmentValue * aim;
+    } else if (pathSegment.startsWith("up ")) {
+      aim -= pathSegmentValue;
+    } else if (pathSegment.startsWith("down ")) {
+      aim += pathSegmentValue;
     }
   }
   return horizontalDistance * depth;
 };
 
-console.log(finalSubmarinePosition(exampleCourseArray));
+console.log(finalSubmarinePosition(examplePathArray));
 
 // --- PART 2 --- //
 
-console.log(finalSubmarinePosition(inputCourseArray));
+console.log(finalSubmarinePosition(inputPathArray));
