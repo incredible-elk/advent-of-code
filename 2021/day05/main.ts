7209375887e379ace8eab42455dfddff3b04472a
Stephanie Geuder <37314329+incredible-elk@users.noreply.github.com>
2021-12-05 17:01:58 +0100

    feat: solution day05 part 1

---
+++ 2021/day05/main.ts
@@ -0,0 +1,77 @@
+// --- EXAMPLE PART 1 --- //
+
+const exampleInput = `
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+`;
+
+const parseInput = (input: string) =>
+  input
+    .trim()
+    .split("\n")
+    .map((line) => line.split(" -> "))
+    .map((line) =>
+      line.map((point) =>
+        point.split(",").map((number) => parseInt(number, 10))
+      )
+    );
+
+const countVentOverlapps = (pointArray: number[][][]) => {
+  const pointCounter: Record<string, number> = {};
+
+  for (let i = 0; i < pointArray.length; i++) {
+    const [[x1, y1], [x2, y2]] = pointArray[i];
+    const linePoints = [];
+
+    if (x1 === x2) {
+      const differenceY = y2 - y1;
+      const numberOfPoints = Math.abs(differenceY) + 1;
+
+      for (let j = 0; j < numberOfPoints; j++) {
+        const y = y1 < y2 ? y1 + j : y1 - j;
+        linePoints.push([x1, y]);
+      }
+    } else if (y1 === y2) {
+      const differenceX = x2 - x1;
+      const numberOfPoints = Math.abs(differenceX) + 1;
+
+      for (let j = 0; j < numberOfPoints; j++) {
+        const x = x1 < x2 ? x1 + j : x1 - j;
+        linePoints.push([x, y1]);
+      }
+    } else {
+      // console.log("diagonal");
+    }
+
+    linePoints.forEach((point) => {
+      const pointString = point.join();
+
+      if (pointCounter[pointString] !== undefined) {
+        pointCounter[pointString]++;
+      } else {
+        pointCounter[pointString] = 1;
+      }
+    });
+  }
+
+  const numberOfOverlappingPoints = Object.values(pointCounter).filter(
+    (number) => number >= 2
+  );
+  return numberOfOverlappingPoints.length;
+};
+
+console.log(countVentOverlapps(parseInput(exampleInput)));
+
+// --- PART 1 --- //
+
+const ventInput = await Deno.readTextFile("./input.txt");
+
+console.log(countVentOverlapps(parseInput(ventInput)));
