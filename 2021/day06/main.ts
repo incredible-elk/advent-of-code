cea540b0901cbdb7319b9cc7f17c1344178db4b6
Stephanie Geuder <37314329+incredible-elk@users.noreply.github.com>
2021-12-09 20:01:00 +0100

    feat: solution day06 part 2

--- 2021/day06/main.ts
+++ 2021/day06/main.ts
@@ -8,26 +8,45 @@ const exampleLanternfish = convertToNumbers(exampleLanternfishString);
 
 console.log(exampleLanternfish);
 
-const lanternfishPopulationGrowth = (array: Array<number>) => {
-  let dailyDecrease = array;
+const lanternfishPopulationGrowth = (array: Array<number>, days: number) => {
+  let counter0 = array.filter((number) => number === 0).length;
+  let counter1 = array.filter((number) => number === 1).length;
+  let counter2 = array.filter((number) => number === 2).length;
+  let counter3 = array.filter((number) => number === 3).length;
+  let counter4 = array.filter((number) => number === 4).length;
+  let counter5 = array.filter((number) => number === 5).length;
+  let counter6 = array.filter((number) => number === 6).length;
+  let counter7 = array.filter((number) => number === 7).length;
+  let counter8 = array.filter((number) => number === 8).length;
 
-  for (let i = 0; i < 80; i++) {
-    const numberOfFertileFish = dailyDecrease.filter(
-      (number) => number === 0
-    ).length;
+  for (let i = 0; i < days; i++) {
+    const numberOfFertileFish = counter0;
 
-    for (let i = 0; i < numberOfFertileFish; i++) {
-      dailyDecrease.push(9);
-    }
-
-    dailyDecrease = dailyDecrease.map((number) => (number === 0 ? 7 : number));
-    dailyDecrease = dailyDecrease.map((number) => number - 1);
+    counter0 = counter1;
+    counter1 = counter2;
+    counter2 = counter3;
+    counter3 = counter4;
+    counter4 = counter5;
+    counter5 = counter6;
+    counter6 = counter7 + numberOfFertileFish;
+    counter7 = counter8;
+    counter8 = numberOfFertileFish;
   }
 
-  return dailyDecrease.length;
+  return (
+    counter0 +
+    counter1 +
+    counter2 +
+    counter3 +
+    counter4 +
+    counter5 +
+    counter6 +
+    counter7 +
+    counter8
+  );
 };
 
-console.log(lanternfishPopulationGrowth(exampleLanternfish));
+console.log(lanternfishPopulationGrowth(exampleLanternfish, 80));
 
 // --- PART 1 --- //
 
@@ -35,4 +54,12 @@ const lanternfishInput = await Deno.readTextFile("input.txt");
 const lanternfishString = lanternfishInput.split(",");
 const lanternfish = convertToNumbers(lanternfishString);
 
-console.log(lanternfishPopulationGrowth(lanternfish));
+console.log(lanternfishPopulationGrowth(lanternfish, 80));
+
+// --- EXAMPLE PART 2 --- //
+
+console.log(lanternfishPopulationGrowth(exampleLanternfish, 256));
+
+// --- PART 2 --- //
+
+console.log(lanternfishPopulationGrowth(lanternfish, 256));
