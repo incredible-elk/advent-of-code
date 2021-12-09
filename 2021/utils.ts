6e655ebe2abc5a00f6bea6f5313d70e6963c4220
Stephanie Geuder <37314329+incredible-elk@users.noreply.github.com>
2021-12-09 20:32:11 +0100

    refactor: rename sumArray variable

--- 2021/utils.ts
+++ 2021/utils.ts
@@ -1,10 +1,10 @@
 export const sumArray = (array: Array<number>) => {
-  let distanceSum = 0;
+  let sum = 0;
 
   for (let i = 0; i < array.length; i++) {
-    distanceSum += array[i];
+    sum += array[i];
   }
-  return distanceSum;
+  return sum;
 };
 
 export const convertToNumbers = (array: Array<string>) =>
