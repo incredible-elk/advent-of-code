5ad62e54247a63783f4b7951df7ba1d4bd613830
Stephanie Geuder <37314329+incredible-elk@users.noreply.github.com>
2021-12-09 20:33:28 +0100

    refactor: clean up code day06

--- 2021/day06/main.ts
+++ 2021/day06/main.ts
@@ -1,49 +1,35 @@
-import { convertToNumbers } from "../utils.ts";
+import { convertToNumbers, sumArray } from "../utils.ts";
+
+// --- UTILS --- //
+
+const parseInput = (input: string) => convertToNumbers(input.split(","));
 
 // --- EXAMPLE PART 1 --- //
 
 const exampleLanternfishInput = "3,4,3,1,2";
-const exampleLanternfishString = exampleLanternfishInput.split(",");
-const exampleLanternfish = convertToNumbers(exampleLanternfishString);
-
-console.log(exampleLanternfish);
+const exampleLanternfish = parseInput(exampleLanternfishInput);
 
 const lanternfishPopulationGrowth = (array: Array<number>, days: number) => {
-  let counter0 = array.filter((number) => number === 0).length;
-  let counter1 = array.filter((number) => number === 1).length;
-  let counter2 = array.filter((number) => number === 2).length;
-  let counter3 = array.filter((number) => number === 3).length;
-  let counter4 = array.filter((number) => number === 4).length;
-  let counter5 = array.filter((number) => number === 5).length;
-  let counter6 = array.filter((number) => number === 6).length;
-  let counter7 = array.filter((number) => number === 7).length;
-  let counter8 = array.filter((number) => number === 8).length;
+  const numberFilter = (x: number) =>
+    array.filter((number) => number === x).length;
 
-  for (let i = 0; i < days; i++) {
-    const numberOfFertileFish = counter0;
+  const counter: number[] = [];
 
-    counter0 = counter1;
-    counter1 = counter2;
-    counter2 = counter3;
-    counter3 = counter4;
-    counter4 = counter5;
-    counter5 = counter6;
-    counter6 = counter7 + numberOfFertileFish;
-    counter7 = counter8;
-    counter8 = numberOfFertileFish;
+  for (let i = 0; i < 9; i++) {
+    counter[i] = numberFilter(i);
   }
 
-  return (
-    counter0 +
-    counter1 +
-    counter2 +
-    counter3 +
-    counter4 +
-    counter5 +
-    counter6 +
-    counter7 +
-    counter8
-  );
+  for (let i = 0; i < days; i++) {
+    const numberOfFertileFish = counter.shift();
+
+    if (numberOfFertileFish === undefined) {
+      throw new Error("numberOfFertileFish is undefined!");
+    }
+    counter[6] += numberOfFertileFish;
+    counter[8] = numberOfFertileFish;
+  }
+
+  return sumArray(counter);
 };
 
 console.log(lanternfishPopulationGrowth(exampleLanternfish, 80));
@@ -51,8 +37,7 @@ console.log(lanternfishPopulationGrowth(exampleLanternfish, 80));
 // --- PART 1 --- //
 
 const lanternfishInput = await Deno.readTextFile("input.txt");
-const lanternfishString = lanternfishInput.split(",");
-const lanternfish = convertToNumbers(lanternfishString);
+const lanternfish = parseInput(lanternfishInput);
 
 console.log(lanternfishPopulationGrowth(lanternfish, 80));
 
