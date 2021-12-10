69d71f3eb598295f5616522bf58b500b3b4203eb
Stephanie Geuder <37314329+incredible-elk@users.noreply.github.com>
2021-12-10 22:38:22 +0100

    feat: solution day10 part 2

--- 2021/day10/main.ts
+++ 2021/day10/main.ts
@@ -94,3 +94,83 @@ const navigationInputString = await Deno.readTextFile("input.txt");
 const navigationInput = parseInput(navigationInputString);
 
 console.log(syntaxErrorScore(navigationInput));
+
+// --- EXAMPLE PART 2 --- //
+
+const syntaxErrorScore2 = (navigationSubsystem: string[][]) => {
+  const totalScoreArray = [];
+
+  for (const line of navigationSubsystem) {
+    const openingCharacters = [];
+    let isCorrupted = false;
+
+    for (let i = 0; i < line.length; i++) {
+      if (
+        line[i] === "(" ||
+        line[i] === "{" ||
+        line[i] === "[" ||
+        line[i] === "<"
+      ) {
+        openingCharacters.push(line[i]);
+      } else if (line[i] === ")") {
+        const lastOpeningCharacter = openingCharacters.pop();
+
+        if (lastOpeningCharacter !== "(") {
+          isCorrupted = true;
+          break;
+        }
+      } else if (line[i] === "]") {
+        const lastOpeningCharacter = openingCharacters.pop();
+
+        if (lastOpeningCharacter !== "[") {
+          isCorrupted = true;
+          break;
+        }
+      } else if (line[i] === "}") {
+        const lastOpeningCharacter = openingCharacters.pop();
+
+        if (lastOpeningCharacter !== "{") {
+          isCorrupted = true;
+          break;
+        }
+      } else if (line[i] === ">") {
+        const lastOpeningCharacter = openingCharacters.pop();
+
+        if (lastOpeningCharacter !== "<") {
+          isCorrupted = true;
+          break;
+        }
+      }
+    }
+    if (!isCorrupted) {
+      const missingClosingCharacters = openingCharacters
+        .map((character) => {
+          if (character === "(") return ")";
+          if (character === "[") return "]";
+          if (character === "{") return "}";
+          if (character === "<") return ">";
+          throw new Error("Error!");
+        })
+        .reverse();
+
+      const points = { ")": 1, "]": 2, "}": 3, ">": 4 };
+      let totalScore = 0;
+
+      for (const character of missingClosingCharacters) {
+        totalScore = totalScore * 5;
+        totalScore += points[character];
+      }
+      totalScoreArray.push(totalScore);
+    }
+  }
+  totalScoreArray.sort((a, b) => a - b);
+  const middleIndex = Math.floor(totalScoreArray.length / 2);
+
+  return totalScoreArray[middleIndex];
+};
+
+console.log(syntaxErrorScore2(exampleNavigationInput));
+
+// --- PART 2 --- //
+
+console.log(syntaxErrorScore2(navigationInput));
