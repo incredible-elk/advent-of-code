961f12885a31081acb2031152b647c12dadd93f1
Stephanie Geuder <37314329+incredible-elk@users.noreply.github.com>
2021-12-12 17:54:04 +0100

    feat: solution day12 part 2

--- 2021/day12/main.ts
+++ 2021/day12/main.ts
@@ -6,6 +6,16 @@ const parseInput = (input: string) =>
     .split("\n")
     .map((connection) => connection.split("-"));
 
+const pathIncludesLowerCaseElementTwice = (path: string[]) => {
+  const lowerCaseElements = path.filter(
+    (element) => element === element.toLowerCase()
+  );
+
+  return lowerCaseElements.some((element, index) =>
+    lowerCaseElements.slice(0, index).includes(element)
+  );
+};
+
 // --- EXAMPLE PART 1 --- //
 
 const exampleCaveMapInput = `
@@ -20,7 +30,7 @@ b-end
 
 const exampleCaveMap = parseInput(exampleCaveMapInput);
 
-const possiblePaths = (caveMap: string[][]) => {
+const possiblePaths = (caveMap: string[][], smallCaveTwice: boolean) => {
   let pathCounter = 0;
 
   const pathStack = [["start"]];
@@ -46,8 +56,14 @@ const possiblePaths = (caveMap: string[][]) => {
     for (const element of possibleNextElements) {
       if (element === "end") {
         pathCounter++;
+      } else if (element === "start") {
+        // discard element
       } else {
-        if (element === element.toUpperCase() || !path.includes(element)) {
+        if (
+          element === element.toUpperCase() ||
+          !path.includes(element) ||
+          (smallCaveTwice && !pathIncludesLowerCaseElementTwice(path))
+        ) {
           const newPath = path.concat([element]);
           pathStack.push(newPath);
         }
@@ -58,7 +74,7 @@ const possiblePaths = (caveMap: string[][]) => {
   return pathCounter;
 };
 
-console.log("exampleCaveMap:", possiblePaths(exampleCaveMap));
+console.log("exampleCaveMap:", possiblePaths(exampleCaveMap, false));
 
 // --- LARGER EXAMPLE INPUT -- //
 
@@ -68,8 +84,8 @@ const exampleCaveMapInputXL = await Deno.readTextFile("exInputXL.txt");
 const exampleCaveMapL = parseInput(exampleCaveMapInputL);
 const exampleCaveMapXL = parseInput(exampleCaveMapInputXL);
 
-console.log("exampleCaveMapL:", possiblePaths(exampleCaveMapL));
-console.log("exampleCaveMapXL:", possiblePaths(exampleCaveMapXL));
+console.log("exampleCaveMapL:", possiblePaths(exampleCaveMapL, false));
+console.log("exampleCaveMapXL:", possiblePaths(exampleCaveMapXL, false));
 
 // --- PART 1 --- //
 
@@ -77,4 +93,15 @@ const caveMapInput = await Deno.readTextFile("input.txt");
 
 const caveMap = parseInput(caveMapInput);
 
-console.log("caveMap:", possiblePaths(caveMap));
+console.log("caveMap:", possiblePaths(caveMap, false));
+
+// --- EXAMPLE PART 2 -- //
+
+console.log("exampleCaveMap:", possiblePaths(exampleCaveMap, true));
+
+console.log("exampleCaveMapL:", possiblePaths(exampleCaveMapL, true));
+console.log("exampleCaveMapXL:", possiblePaths(exampleCaveMapXL, true));
+
+// --- PART 2 -- //
+
+console.log("caveMap:", possiblePaths(caveMap, true));
