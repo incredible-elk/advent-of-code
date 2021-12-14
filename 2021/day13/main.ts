616f16436e683d3f8b62e08899e5c21390282384
Stephanie Geuder <37314329+incredible-elk@users.noreply.github.com>
2021-12-14 01:13:59 +0100

    feat: solution day13 part 2

--- 2021/day13/main.ts
+++ 2021/day13/main.ts
@@ -55,16 +55,14 @@ fold along x=5
 
 const exampleParsed = parseInput(exampleManualInput);
 
-const visibleDotsAfterFold = (input: {
-  dotCoordinates: number[][];
-  foldInstructions: {
+const visibleDotsAfterFold = (
+  dotCoordinates: number[][],
+  fold: {
     letter: string;
     number: number;
-  }[];
-}) => {
-  const fold = input.foldInstructions[0];
-
-  const dotsAfterFold = input.dotCoordinates.map(([x, y]) => {
+  }
+) => {
+  const dotsAfterFold = dotCoordinates.map(([x, y]) => {
     if (fold.letter === "y") {
       if (y > fold.number) {
         const differenceY = y - fold.number;
@@ -92,10 +90,23 @@ const visibleDotsAfterFold = (input: {
         .some((dot2) => dot[0] === dot2[0] && dot[1] === dot2[1])
   );
 
-  return dotsWithoutDuplicates.length;
+  return dotsWithoutDuplicates;
 };
 
-console.log(visibleDotsAfterFold(exampleParsed));
+const visibleDotsPart1 = (input: {
+  dotCoordinates: number[][];
+  foldInstructions: {
+    letter: string;
+    number: number;
+  }[];
+}) => {
+  const fold = input.foldInstructions[0];
+
+  const dotsAfterFold = visibleDotsAfterFold(input.dotCoordinates, fold);
+  return dotsAfterFold.length;
+};
+
+console.log(visibleDotsPart1(exampleParsed));
 
 // --- PART 1 --- //
 
@@ -103,4 +114,43 @@ const manualInput = await Deno.readTextFile("input.txt");
 
 const parsedInput = parseInput(manualInput);
 
-console.log(visibleDotsAfterFold(parsedInput));
+console.log(visibleDotsPart1(parsedInput));
+
+// --- EXAMPLE PART 2 --- //
+
+const visibleDotsPart2 = (input: {
+  dotCoordinates: number[][];
+  foldInstructions: {
+    letter: string;
+    number: number;
+  }[];
+}) => {
+  let dots = input.dotCoordinates;
+
+  for (let i = 0; i < input.foldInstructions.length; i++) {
+    const fold = input.foldInstructions[i];
+
+    dots = visibleDotsAfterFold(dots, fold);
+  }
+
+  const allXes = dots.map((dot) => dot[0]);
+  const allYs = dots.map((dot) => dot[1]);
+
+  const maxX = Math.max(...allXes);
+  const maxY = Math.max(...allYs);
+
+  for (let y = 0; y <= maxY; y++) {
+    let string = "";
+    for (let x = 0; x <= maxX; x++) {
+      const isInDots = dots.some((dot) => dot[0] === x && dot[1] === y);
+      string += isInDots ? "#" : ".";
+    }
+    console.log(string);
+  }
+};
+
+console.log(visibleDotsPart2(exampleParsed));
+
+// --- PART 2 --- //
+
+console.log(visibleDotsPart2(parsedInput));
